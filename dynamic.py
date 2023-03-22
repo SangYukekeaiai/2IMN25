d4d31e00bc944c85709c6a4f4ef393fa69669788
YYds <yuyang990421@gmail.com>
2023-03-22 12:51:21 +0100

    add test and fix some bug

--- dynamic.py
+++ dynamic.py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 import collections
 
 class Dynamic():
-    def __init__(self, G, immune_time, infect_rate, infect_time, death_rate, death_time, recover_rate, begin_infected_number):
+    def __init__(self, G, immune_time, infect_rate, infect_time, death_rate, death_time, recover_time, begin_infected_number):
         self.G = G
         self.immune_time = immune_time
         self.infect_rate = infect_rate
         self.infect_time = infect_time
         self.death_rate = death_rate
-        self.recover_rate = recover_rate
+        self.recover_time = recover_time
         self.death_time = death_time
         self.begin_infected_number = begin_infected_number
 
@@ -24,44 +24,50 @@ class Dynamic():
             if n in infected:
                 self.G.nodes[n]['status'] = 'infected'
                 self.G.nodes[n]['day_to_change_state'] = self.immune_time
+                self.G.nodes[n]['future'] = 'immune'
+            else:
+                self.G.nodes[n]['status'] = 'healthy'
+                self.G.nodes[n]['day_to_change_state'] = 0
+                self.G.nodes[n]['future'] = 'healthy'
+
 
 
     def updat_time(self):
-        for node in self.G.__iter__:
-            if self.G.nodes[node]['status'] != 'death' & self.G.nodes[node]['day_to_change_state'] != 0:
+        for node in self.G.__iter__():
+            if self.G.nodes[node]['status'] != 'death' and self.G.nodes[node]['day_to_change_state'] != 0:
                 self.G.nodes[node]['day_to_change_state'] -= 1
 
     def death_event(self):
-        for node in self.G.__iter__:
-            if self.G.nodes[node]['status'] != 'infected' & (
-                self.G.nodes[node]['future'] == 'death' & self.G.nodes[node]['day_to_change_state'] == 0):
+        for node in self.G.__iter__():
+            if self.G.nodes[node]['status'] != 'infected' and (
+                self.G.nodes[node]['future'] == 'death' and self.G.nodes[node]['day_to_change_state'] == 0):
                 self.G.nodes[node]['status'] = 'death'
 
     def recovery(self):
-        for node in self.G.__iter__:
-            if self.G.nodes[node]['status'] == 'infected' & (
-                    self.G.nodes[node]['future'] == 'immune' & self.G.nodes[node]['day_to_change_state'] == 0):
+        for node in self.G.__iter__():
+            if self.G.nodes[node]['status'] == 'infected' and (
+                    self.G.nodes[node]['future'] == 'immune' and self.G.nodes[node]['day_to_change_state'] == 0):
                 self.G.nodes[node]['status'] = 'immune'
                 self.G.nodes[node]['future'] = 'healthy'
                 self.G.nodes[node]['day_to_change_state'] = self.immune_time
 
     def infaction(self):
-        for node in self.G.__iter__:
+        for node in self.G.__iter__():
             if self.G.nodes[node]['status'] == 'infected':
                 victim_list = []
                 for neighbor in list(self.G.neighbors(node)):
-                    if self.G.nodes[neighbor]['status'] == 'healthy' & self.G.nodes[neighbor]['future'] == 'healthy':
+                    if self.G.nodes[neighbor]['status'] == 'healthy' and self.G.nodes[neighbor]['future'] == 'healthy':
                         victim_list.append(neighbor)
                 destiny = np.random.binomial(1, self.infect_rate, len(victim_list))
-                for i in len(destiny):
+                for i in range(len(destiny)):
                     if destiny[i] == 1:
                         self.G.nodes[victim_list[i]]['future'] = 'infected'
                         self.G.nodes[victim_list[i]]['day_to_change_state'] = self.infect_time
 
     def be_infected(self):
-        for node in self.G.__iter__:
-            if self.G.nodes[node]['status'] == 'healthy' & (
-                    self.G.nodes[node]['future'] == 'infected' & self.G.nodes[node]['day_to_change_state'] == 0):
+        for node in self.G.__iter__():
+            if self.G.nodes[node]['status'] == 'healthy' and (
+                    self.G.nodes[node]['future'] == 'infected' and self.G.nodes[node]['day_to_change_state'] == 0):
                 self.G.nodes[node]['status'] = 'infected'
                 destiny = np.random.choice(np.arange(0, 2), p=[1-self.death_rate, self.death_rate])
                 if destiny:
@@ -73,12 +79,31 @@ class Dynamic():
 
 
     def quit_immune(self):
-        for node in self.G.__iter__:
-            if self.G.nodes[node]['status'] == 'immune' & (
-                    self.G.nodes[node]['future'] == 'healthy' & self.G.nodes[node]['day_to_change_state'] == 0):
+        for node in self.G.__iter__():
+            if self.G.nodes[node]['status'] == 'immune' and (
+                    self.G.nodes[node]['future'] == 'healthy' and self.G.nodes[node]['day_to_change_state'] == 0):
                 self.G.nodes[node]['status'] = 'healthy'
                 self.G.nodes[node]['future'] = 'healthy'
                 self.G.nodes[node]['day_to_change_state'] = 0
+
+    def record_print(self):
+        death_nodes = []
+        infected_nodes = []
+        recovered_nodes = []
+        healthy_nodes = []
+        for node in self.G.__iter__():
+            if self.G.nodes[node]['status'] == 'immune':
+                recovered_nodes.append(node)
+            if self.G.nodes[node]['status'] == 'healthy':
+                healthy_nodes.append(node)
+            if self.G.nodes[node]['status'] == 'death':
+                death_nodes.append(node)
+            if self.G.nodes[node]['status'] == 'infected':
+                infected_nodes.append(node)
+        print("death node:", death_nodes)
+        print("infected node:", infected_nodes)
+        print("recovered node:", recovered_nodes)
+        print("healthy node:", healthy_nodes)
 
 
     def dayrun(self):
--- generate_network.py
+++ generate_network.py
@@ -40,9 +40,9 @@ class Social_Net():
 			#print 'Adding', int(round(val/(j+1))), 'cliques of size', j+1
 			for k in range(int(round(val/(j+1)))):
 				self.add_clique(j+1, G)
-		print('Family!')
-		self.degree_histogram(
-			G=self.G, hist_file='family_hist.png', loglog_file='family_log.png')
+		# print('Family!')
+		# self.degree_histogram(
+		# 	G=self.G, hist_file='family_hist.png', loglog_file='family_log.png')
 
 	def workplace_BA(self):
 		#creates a scale-free network on non-essential workers
@@ -53,9 +53,9 @@ class Social_Net():
 				n_work += 1
 				working_nodes.append(n)
 		BAG = nx.barabasi_albert_graph(n_work, self.ba_degree)
-		print('Workplace BA!')
-		self.degree_histogram(G=BAG, hist_file='workBA_hist.png',
-		                      loglog_file='workBA_log.png')
+		# print('Workplace BA!')
+		# self.degree_histogram(G=BAG, hist_file='workBA_hist.png',
+		#                       loglog_file='workBA_log.png')
 		#map the BAG to the actual network edges
 		for pair in list(BAG.edges()):
 			self.G.add_edge(working_nodes[pair[0]],
@@ -76,11 +76,11 @@ class Social_Net():
 		#print 'Number of essential people is', n_ess
 		#ER = nx.erdos_renyi_graph(n_ess, self.essential_connection)
 		m = int(float(n_ess*self.rand_degree)/2)
-		print('m is', m)
+		# print('m is', m)
 		ER = nx.gnm_random_graph(n_ess, m)
-		print('Workplace Random!')
-		self.degree_histogram(G=ER, hist_file='workRand_hist.png',
-		                      loglog_file='workRand_log.png')
+		# print('Workplace Random!')
+		# self.degree_histogram(G=ER, hist_file='workRand_hist.png',
+		#                       loglog_file='workRand_log.png')
 		#map the ER to the actual network edges
 		for pair in list(ER.edges()):
 			self.G.add_edge(essential_nodes[pair[0]],
@@ -108,9 +108,9 @@ class Social_Net():
 				self.G[n][m]['relation'] = 'ess_non'
 				newG.add_edge(n, m)
 
-		print('interaction!')
-		self.degree_histogram(G=newG, hist_file='inter_hist.png',
-		                      loglog_file='inter_log.png')
+		# print('interaction!')
+		# self.degree_histogram(G=newG, hist_file='inter_hist.png',
+		#                       loglog_file='inter_log.png')
 
 	def social(self):
 		socG = nx.Graph()
@@ -123,9 +123,9 @@ class Social_Net():
 			self.G.add_edge(i, j, lockdown=False)
 			self.G[i][j]['relation'] = 'soical'
 			socG.add_edge(i, j)
-		print('Social!')
-		self.degree_histogram(G=socG, hist_file='soc_hist.png',
-		                      loglog_file='soc_log.png')
+		# print('Social!')
+		# self.degree_histogram(G=socG, hist_file='soc_hist.png',
+		#                       loglog_file='soc_log.png')
 
 	def set_parameters(self, family_sizes=[0.3, 0.35, 0.18, 0.17], workrate=0.7, essential=0.2, ba_degree=3, essential_connection=0.6, interaction_prob=0.20, social_prob=0.001, rand_degree=5):
 		self.workrate = workrate
@@ -254,8 +254,8 @@ if __name__ == '__main__':
 	#My.interaction()
 	#My.social()
 	G = My.return_graph()
-	print('Overall')
-	My.degree_histogram(loglog_file='temp_log.png')
+	# print('Overall')
+	# My.degree_histogram(loglog_file='temp_log.png')
 	#print G.nodes(data=True)
 	#print G.edges()
 	My.draw_graph()
