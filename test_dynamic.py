8dd8483d20e4d7d87d7319991f1e61e54405978b
YYds <yuyang990421@gmail.com>
2023-03-23 07:27:33 +0100

    add distribution

--- test_dynamic.py
+++ test_dynamic.py
@@ -6,15 +6,17 @@ import time
 
 SN = Social_Net(complete_net=False)
 SN.set_parameters(ba_degree=2, social_prob=0.025, rand_degree=25)
-SN.start_network(2000)
+SN.start_network(5000)
 G = SN.return_graph()
 # SN.draw_graph()
 
-spread = Dynamic(G=G, immune_time=5, infect_rate=0.2, infect_time=14, death_rate=0.01, death_time=14, recover_time=14, begin_infected_number=50)
+spread = Dynamic(G=G, immune_time=5, infect_rate=0.2, infect_time=14, death_rate=0.01, death_time=14, recover_time=14, begin_infected_number=5)
 
 spread.init_Graph_state()
 
-for i in range(100):
-    print("iteration ", i, ": ")
-    spread.dayrun()
-    spread.record_print()
+# for i in range(100):
+#     print("iteration ", i, ": ")
+#     spread.dayrun()
+#     spread.record_print()
+
+spread.draw_distribution(1000)
